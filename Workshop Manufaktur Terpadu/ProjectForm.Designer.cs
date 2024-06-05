25663099519e366c4557a912a5a24686fe4a616c
primaaldrin <primaaldrinh@gmail.com>
2024-06-06 02:42:25 +0700

    Added PLC send test component

--- Workshop Manufaktur Terpadu/ProjectForm.Designer.cs
+++ Workshop Manufaktur Terpadu/ProjectForm.Designer.cs
@@ -28,12 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.serialPortComboBox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.connect_button = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.plcOnTestButton = new System.Windows.Forms.Button();
+            this.plcOffTestButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -74,6 +80,7 @@
             this.connect_button.TabIndex = 2;
             this.connect_button.Text = "Connect";
             this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.connect_button_Click);
             // 
             // button1
             // 
@@ -83,17 +90,50 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "Close";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.plcOffTestButton);
+            this.groupBox2.Controls.Add(this.plcOnTestButton);
+            this.groupBox2.Location = new System.Drawing.Point(187, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(169, 156);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "PLC Test";
+            // 
+            // plcOnTestButton
+            // 
+            this.plcOnTestButton.Location = new System.Drawing.Point(6, 19);
+            this.plcOnTestButton.Name = "plcOnTestButton";
+            this.plcOnTestButton.Size = new System.Drawing.Size(157, 59);
+            this.plcOnTestButton.TabIndex = 0;
+            this.plcOnTestButton.Text = "ON";
+            this.plcOnTestButton.UseVisualStyleBackColor = true;
+            // 
+            // plcOffTestButton
+            // 
+            this.plcOffTestButton.Location = new System.Drawing.Point(6, 91);
+            this.plcOffTestButton.Name = "plcOffTestButton";
+            this.plcOffTestButton.Size = new System.Drawing.Size(157, 59);
+            this.plcOffTestButton.TabIndex = 1;
+            this.plcOffTestButton.Text = "OFF";
+            this.plcOffTestButton.UseVisualStyleBackColor = true;
             // 
             // ProjectForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(412, 180);
+            this.ClientSize = new System.Drawing.Size(374, 180);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "ProjectForm";
             this.Text = "Final Project: Truck\'s Gate Solution";
+            this.Load += new System.EventHandler(this.ProjectForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -105,5 +145,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox serialPortComboBox;
         private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button plcOnTestButton;
+        private System.Windows.Forms.Button plcOffTestButton;
     }
 }
--- Workshop Manufaktur Terpadu/ProjectForm.cs
+++ Workshop Manufaktur Terpadu/ProjectForm.cs
@@ -16,5 +16,28 @@ namespace Workshop_Manufaktur_Terpadu
         {
             InitializeComponent();
         }
+
+        private void ProjectForm_Load(object sender, EventArgs e)
+        {
+            String[] portList = System.IO.Ports.SerialPort.GetPortNames();
+            foreach (String portName in portList)
+            {
+                serialPortComboBox.Items.Add(portName);
+            }
+        }
+
+        private void connect_button_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = serialPortComboBox.Text;
+            serialPort1.Open();
+
+            // Please change this later
+            ProjectForm.ActiveForm.Text = serialPort1.PortName + " is connected";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+        }
     }
 }
