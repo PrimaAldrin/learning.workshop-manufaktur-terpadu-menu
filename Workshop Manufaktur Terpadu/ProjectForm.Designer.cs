554c8f48ec97d395e21875a0cc25e661fcafbdde
primaaldrin <primaaldrinh@gmail.com>
2024-06-06 02:31:48 +0700

    Added some connectivity components

--- Workshop Manufaktur Terpadu/ProjectForm.Designer.cs
+++ Workshop Manufaktur Terpadu/ProjectForm.Designer.cs
@@ -29,16 +29,60 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.serialPortComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.connect_button);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.serialPortComboBox);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 156);
+            this.groupBox1.Size = new System.Drawing.Size(169, 156);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Connectivity";
+            // 
+            // serialPortComboBox
+            // 
+            this.serialPortComboBox.FormattingEnabled = true;
+            this.serialPortComboBox.Location = new System.Drawing.Point(75, 19);
+            this.serialPortComboBox.Name = "serialPortComboBox";
+            this.serialPortComboBox.Size = new System.Drawing.Size(88, 21);
+            this.serialPortComboBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Serial Port";
+            // 
+            // connect_button
+            // 
+            this.connect_button.Location = new System.Drawing.Point(6, 52);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(157, 46);
+            this.connect_button.TabIndex = 2;
+            this.connect_button.Text = "Connect";
+            this.connect_button.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // ProjectForm
             // 
@@ -48,6 +92,8 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "ProjectForm";
             this.Text = "Final Project: Truck\'s Gate Solution";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -55,5 +101,9 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button connect_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox serialPortComboBox;
+        private System.Windows.Forms.Button button1;
     }
 }
