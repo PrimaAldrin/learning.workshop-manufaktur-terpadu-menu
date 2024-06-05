3c663556d32a7d14e82c2b06c789530582aae752
primaaldrin <primaaldrinh@gmail.com>
2024-06-06 01:44:42 +0700

    Initialize a Main Form file

--- Workshop Manufaktur Terpadu/Program.cs
+++ Workshop Manufaktur Terpadu/Program.cs
@@ -16,7 +16,7 @@ namespace Workshop_Manufaktur_Terpadu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
