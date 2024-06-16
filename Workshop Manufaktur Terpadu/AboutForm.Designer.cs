df755d18901ae4bc907882c11f347a774f04d0a4
primaaldrin <primaaldrinh@gmail.com>
2024-06-16 20:31:05 +0700

    The scenario completed

--- Workshop Manufaktur Terpadu/AboutForm.Designer.cs
+++ Workshop Manufaktur Terpadu/AboutForm.Designer.cs
@@ -56,6 +56,7 @@
             this.linkLabel1.TabIndex = 2;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Visit GitHub";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // label1
             // 
--- Workshop Manufaktur Terpadu/AboutForm.cs
+++ Workshop Manufaktur Terpadu/AboutForm.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -25,6 +27,11 @@ namespace Workshop_Manufaktur_Terpadu
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://github.com/primaaldrin/learning.workshop-manufaktur-terpadu-menu") { UseShellExecute = true });
         }
     }
 }
